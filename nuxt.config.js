31eedd157ac8804fa8c75f4b62ccda007ee3c184
Amanda Walsh <rock.amanda5@gmail.com>
2022-08-29 22:07:59 -0400

    change router config for github pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,6 +7,11 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+  // Build to repo name for GitHub Pages
+  router: {
+    base: '/glider/'
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - glider',
