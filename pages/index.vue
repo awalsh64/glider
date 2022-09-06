63e47a3e46d4c110aac258edb39850da4b484fea
Amanda Walsh <rock.amanda5@gmail.com>
2022-09-05 21:37:00 -0400

    config eslint prettier settings

--- pages/index.vue
+++ pages/index.vue
@@ -11,29 +11,30 @@
         </v-card-title>
         <v-card-text>
           <p>
-            Import NetCDF file to view glider path and import .wav file to view associated spectrogram.
+            Import NetCDF file to view glider path and import .wav file to view
+            associated spectrogram.
           </p>
-         </v-card-text>
-         <v-card-actions>
+        </v-card-text>
+        <v-card-actions>
           <v-spacer />
           <v-btn color="primary" nuxt to="/import-files"> Import </v-btn>
         </v-card-actions>
       </v-card>
       <v-card>
         <div id="app" style="height: 50vh">
-          <chart-data :points="points"/>
-         </div>
+          <chart-data :points="points" />
+        </div>
       </v-card>
     </v-col>
   </v-row>
 </template>
 
 <script>
-  import ChartData from '@/components/chart.vue'
+import ChartData from '@/components/chart.vue'
 export default {
   name: 'IndexPage',
-  components:{
-    ChartData
+  components: {
+    ChartData,
   },
   data: () => {
     return {
@@ -43,11 +44,11 @@ export default {
         { x: 1, y: 7 },
         { x: 2, y: 3 },
         { x: 3, y: 10 },
-        { x: 4, y:  12},
+        { x: 4, y: 12 },
         { x: 5, y: 9 },
-        { x: 6, y: 15 }
-      ]
+        { x: 6, y: 15 },
+      ],
     }
-    },
+  },
 }
 </script>
