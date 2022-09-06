c65374cff49717bf8f47c12f9ed3361f59c1cc06
Amanda Walsh <rock.amanda5@gmail.com>
2022-09-05 20:29:13 -0400

    add 3d plot

--- pages/index.vue
+++ pages/index.vue
@@ -19,12 +19,35 @@
           <v-btn color="primary" nuxt to="/import-files"> Import </v-btn>
         </v-card-actions>
       </v-card>
+      <v-card>
+        <div id="app" style="height: 50vh">
+          <chart-data :points="points"/>
+         </div>
+      </v-card>
     </v-col>
   </v-row>
 </template>
 
 <script>
+  import ChartData from '@/components/chart.vue'
 export default {
   name: 'IndexPage',
+  components:{
+    ChartData
+  },
+  data: () => {
+    return {
+      // Data points for the chart
+      points: [
+        { x: 0, y: 0 },
+        { x: 1, y: 7 },
+        { x: 2, y: 3 },
+        { x: 3, y: 10 },
+        { x: 4, y:  12},
+        { x: 5, y: 9 },
+        { x: 6, y: 15 }
+      ]
+    }
+    },
 }
 </script>
